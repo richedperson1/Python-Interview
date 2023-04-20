416ecdee9481e24099b340095c863630c6050201
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-04-20 14:58:45 +0530

    Adding iterator

---
+++ Advance Python/iterator Generator iterable/iterator_iterable.py
@@ -0,0 +1,25 @@
+
+"""
+Itarators : 
+
+Those object where we can able to apply iter function is called as iterator.
+
+"""
+
+first = "Rutvik Jaiswal"
+
+objIter = iter(first)
+
+print(next(objIter))
+print(next(objIter))
+print(next(objIter))
+print(next(objIter))
+print(next(objIter))
+print(next(objIter))
+
+
+set_D = set([2, 5, 8, 93])
+first = iter(set_D)
+print(set_D)
+print(first)
+print(next(first))
