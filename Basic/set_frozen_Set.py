c41793cd7dae9a0c84958c1365bb2bd6b8a3bcca
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-04-22 21:00:19 +0530

    set and frozenset

---
+++ Basic/set_frozen_Set.py
@@ -0,0 +1,30 @@
+"""
+Set vs Frozenset
+
+"""
+
+
+"""
+Similarity
+
+1. It contain only unique value
+2. Lookuptime is O(1)
+3. Iterable object
+"""
+
+
+"""
+desimilarity
+
+1. Immutability : Frozenset is immutable
+2. Hashing availble for frozenset as it fixed size
+"""
+
+
+# Frozen set can be used as Dictionary keys
+dist = {}
+arr = [1, 2, 3, 4, 4]
+FrozeArr = frozenset(arr)
+
+dist[frozenset] = 5
+print(dist)
