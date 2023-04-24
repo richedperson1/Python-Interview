b42dffae48a530b2e8b39c8d59f8580b289bfc15
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-04-24 13:31:48 +0530

    adding jinja templetes basic

---
+++ Flask Interview Quection/Jinja templates/app.py
@@ -0,0 +1,46 @@
+"""
+Getting know about jinja templates
+
+1. templates engine : 
+
+    a. These are the files that contain static data along with placeholder 
+    for dynamic data.
+    b. It's rendered HTML file with specific data
+
+    Example of templates
+        i.   Chameleon
+        ii.  Cheetah     
+        iii. Jinja
+        iv.  Juno     
+"""
+
+
+"""
+Jinja Templates
+
+1. It look like python only
+2. Special delimiters are present in jinja so that every expression writen in right way
+
+    a. {{}}  : Expression for output [act as placeholder for value]
+    b. {% %} : Expression for control flow statement like [if ,else, for]
+"""
+
+
+from flask import *
+app = Flask("__main__")
+
+
+@app.route("/")
+def startPage():
+    return render_template("index.html")
+
+
+@app.route("/table", methods=["post"])
+def printTable():
+    if request.method == "POST":
+        tableNum = int(request.form["tableNum"])
+        return render_template("table.html", number=tableNum)
+    return "Nope"
+
+
+app.run(debug=True)
