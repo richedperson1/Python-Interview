9da20937b83c7410fb6e4f608672ada5673e464d
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-04-24 12:53:22 +0530

    adding routing methods (API methods)

--- Flask Interview Quection/routingMethods/app.py
+++ Flask Interview Quection/routingMethods/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, render_template
 
 
 app = Flask("__main__")
@@ -11,6 +11,19 @@ def mainPage():
     }
 
     return make_response("This response making using make_response api", 202, headers)
+
+
+# If we are giving post method then user need to give only post method
+# for routing
+@app.route("/helloworld", methods=["post", "get"])
+def startUsingHello():
+    return "<h1>Hello world</h1>"
+# By default get method is present in the API
+
+
+@app.route("/home")
+def homePage():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
