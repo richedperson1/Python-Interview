90828f05fd1e836c23bc4a97751e37e060ea3a90
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-04-26 15:49:05 +0530

    adding control statment in jinja

--- Flask Interview Quection/jinja_inheritance/app.py
+++ Flask Interview Quection/jinja_inheritance/app.py
@@ -13,5 +13,14 @@ def indexPage():
     return render_template("index.html")
 
 
+@app.route("/template_var")
+def template_var():
+    password1 = "Rutvik123654987"
+    name1 = "Rutvik"
+    userName1 = ["Rutvik_Jaiswal", "Rutvik"]
+    contain = {"password": password1, "name": name1, "userName": userName1}
+    return render_template("control_statement.html", context=contain)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
