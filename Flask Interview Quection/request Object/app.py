dd7624294d3257104bc9f04c924df48ef852908f
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-04-30 16:30:46 +0530

    adding json request method in flask

--- Flask Interview Quection/request Object/app.py
+++ Flask Interview Quection/request Object/app.py
@@ -1,6 +1,6 @@
 from flask import *
 
-
+import requests
 app = Flask("__main__")
 
 
@@ -16,6 +16,28 @@ def requestDemo():
 def requestForm():
     return render_template("inputData.html")
     # print("----->", rquest["query_string"])
+
+
+# JSON File
+@app.route("/jsoninput")
+def jsonInput():
+    person = {
+        "name": "Rutvik",
+        "lastName": "Jaiswal",
+        "Mobile No.": 9673062604,
+        "skills": ["python", "JS", "CSS/HTML", "MySQL"]
+    }
+    final = requests.post("http://127.0.0.1:5000/jsonProcedure", json=person)
+    return str(final.text)+"Hello world"
+
+
+@app.route("/jsonProcedure", methods=["post"])
+def jsonProcedure():
+    if request.is_json:
+        print("Json found")
+        return f"Hello world {request.json}"
+    else:
+        return "Data not founds"
 
 
 if __name__ == "__main__":
