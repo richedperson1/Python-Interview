2919d091c870d046e9da73a281423065c2fab116
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-05-09 15:59:26 +0530

    adding create and update operation in the Flask sqlite

--- Flask Interview Quection/challenges/app.py
+++ Flask Interview Quection/challenges/app.py
@@ -5,7 +5,7 @@ using Cookie as well as Flask-Session)
 
 """
 
-from flask import Flask,render_template,session,make_response,redirect,url_for
+from flask import Flask,render_template,session,make_response,redirect,url_for,request
 
 
 app = Flask(__name__)
@@ -27,15 +27,20 @@ def countingPageView():
     return render_template("count.html")
 
 
-@app.route("/hello_fun",methods = ["post"])
-def heelo():
-    return "Hellow"
-
-@app.route("/reponse")
+@app.route("/countCookies")
 def make_response_Func():
-    a = make_response(redirect(url_for("heelo")))
-    
-    return "Hello "
+    # Get the current count from the cookie
+    cookie_count = request.cookies.get('counting')
+    if cookie_count is None:
+        cookie_count = 0
+    else:
+        cookie_count = int(cookie_count)
 
-app.run()
+    # Increment the count and set the cookie
+    cookie_count += 1
+    res = make_response(render_template("count_cookies.html"))
+    res.set_cookie("counting", str(cookie_count))
+
+    return res
+# app.run()
 
---
+++ Flask Interview Quection/database Connection/app.py
@@ -0,0 +1,72 @@
+from flask import (Flask,request,render_template,redirect,session,url_for)
+import sqlite3
+
+app = Flask(__name__)
+
+app.secret_key = "asjdfkjasndckjshdf"
+
+connection = sqlite3.connect("first.db")
+
+# connection.execute("DROP TABLE STUDENT")
+connection.execute("CREATE TABLE IF NOT EXISTS STUDENT(email TEXT,first_name TEXT,last_name TEXT,password text)")
+
+
+
+@app.route("/")
+def homePage():
+    if "username" in session:
+        return "Home page"
+    return redirect(url_for("loginPage"))
+
+@app.route("/login")
+def loginPage():
+    return render_template("login.html")
+
+@app.route("/authetication",methods = ["post"])
+def autheticationPage():
+    userEmail = request.form.get("EmailName")
+    password = request.form.get("PasswordName")
+    final = ""
+    with sqlite3.connect("first.db") as conn:
+        cursor = conn.cursor()
+        query = f"select * from STUDENT where email =='{userEmail}' and password = '{password}'"
+        cursor.execute(query)
+        final = cursor.fetchall()
+    if len(final)>0:
+        session["username"] = userEmail
+        return redirect(url_for("homePage"))
+    
+    return redirect(url_for("loginPage"))
+
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
+
+@app.route("/addingData",methods = ["post"])
+def addingStudent():
+    if request.method=="POST":
+        # try:
+            emailId   = request.form.get("email")
+            firstName = request.form.get("first-name")
+            LastName  = request.form.get("last-name")
+            password  = request.form.get("password")
+            print(emailId,firstName,LastName,password)
+            with sqlite3.connect("first.db") as conn:
+                cursor = conn.cursor()
+                query = f"""
+                        INSERT INTO STUDENT 
+                        (email, first_name, last_name, password) VALUES 
+                        ('{emailId}', '{firstName}', '{LastName}', 
+                        '{password}');
+                        """
+                print(query)
+                cursor.execute(query)
+                conn.commit()
+            
+            return redirect(url_for("loginPage"))
+        # except:
+        #     return  "not able to load data into the database"
+    return "Not found"
+       
+if __name__=="__main__":
+    app.run(debug=True,port=4000)
