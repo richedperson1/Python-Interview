f3629f33904d997f130c8c8ec1280c635c8e5686
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-05-16 14:36:55 +0530

    Dictionary key modification

--- Advance Python/Dictionary/Can you add List as Dictionary Key in Python.py
+++ Advance Python/Dictionary/Can you add List as Dictionary Key in Python.py
@@ -22,6 +22,17 @@ print(hash(tupple))
 dist[tupple] = 5
 print(dist)
 
+"""
+Tuple will be hashable if tuple element itself is hashable 
+Ex. if Tuple contain any data type which has modified after change then it was not hashable
+
+a = tuple([1,2,3])
+
+"a" has hash function return value 
+
+b = tuple([[1,2,3],4])
+"b" not be hash function return value
+"""
 
 # Dictionary can't be hashable so can't used as key to the dictionary itself
 dictionary = {"Rutvik": "jaiswal"}
