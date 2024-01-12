515288054827e34a083f5ea31752976ebb000348
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2024-01-12 12:10:15 +0530

    adding test

--- Flask Interview Quection/ORM/test.py
+++ Flask Interview Quection/ORM/test.py
@@ -1,38 +1,31 @@
-from orm import folder_info
+from flask import current_app
+from orm import chat_info
 from db import db
 import time
 import random
 
+def data_storage(data):
+    print("Data is : ",data)
 
 def addingFolder_streaming():
-    
     def streaming():
         ind = 5
-        while ind>0:
-            ind-=1
-            yield(f"index-{ind}")
-            time.sleep(1)
-            
-    for data in streaming():
-        print(f"{data}")
-        yield f"{data}\n"
-    
-    return "found"
+        while ind > 0:
+            ind -= 1
+            yield f"index-{ind}"
+            time.sleep(0.2)
 
-def addingFolder(folder_name,email_id):
+    final_data = ''
+    for data in streaming():
+        new_data_stream = data + str(random.random())
+        yield f"{new_data_stream}\n"
+        final_data += f"{new_data_stream}"
+
+    data_storage(final_data)
+
+def addingFolder(folder_name, email_id, obj):
     new_data = random.random()
     folder_name = f"{folder_name}-{new_data}"
     email_id = f"{email_id}-{new_data}"
-    
-    
-    data_storage(folder_name,email_id)
+
     return addingFolder_streaming()
-
-
-def data_storage(folder_name,email_id):
-    print("storing data : ")
-    new_folder_info = folder_info(folder_name = folder_name,email_id = email_id)
-    
-    db.session.add(new_folder_info)
-    db.session.commit()
-    
