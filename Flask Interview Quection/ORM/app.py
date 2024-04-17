4f962e5041db6117842b5094bd32736591c63543
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2024-04-17 21:30:53 +0530

    adding streaming in saving data into db using orm

--- Flask Interview Quection/ORM/app.py
+++ Flask Interview Quection/ORM/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify, request,jsonify
+from flask import Flask, jsonify, request,jsonify,Response,stream_with_context
 from flask_sqlalchemy import SQLAlchemy
 import random
+import time
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pets.db'  # SQLite database file
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 app.app_context().push()
@@ -11,6 +11,7 @@ app.app_context().push()
 class Owner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
+    details = db.Column(db.Text)
 
     # pets = db.relationship('Pet', backref='owner_info_backref')
 
@@ -22,8 +23,27 @@ class Pet(db.Model):
 
     Owner_backref = db.relationship('Owner', backref='owner_info_backref')
 
-# rocky = Pet(id,name = 'rock',age,Rutvik)
-# rucky.name
+
+
+class streaming_data:
+    def generate_data(self):
+        self.stream = ""
+        for i in range(10):
+            time.sleep(1)  # Simulate data generation
+            yield f"Data chunk {i+1}\n"
+            self.stream+=f"Data chunk {i+1}\n"
+        self.storing_data_db()
+    def storing_data_db(self):
+        new_owner = Owner(id = 5,name = "Rutvik",details = self.stream)
+        
+        db.session.add(new_owner)
+        db.session.commit()
+        
+@app.route('/stream')
+def stream_response():
+    obj = streaming_data()
+    return Response(stream_with_context(obj.generate_data()), content_type='text/plain')
+
 
 db.create_all()
 
